8a3adea96dfe13227a80b76b3da413c6fe11d1d5
danik <danikdevs@gmail.com>
2025-05-23 22:30:22 +0500

    preparation

---
+++ src/test/java/src/danik/tasksolution/TaskSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package src.danik.tasksolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
